47dc48011bf3feded12c2c06fc29a3f9e693faf3
shuro4ka <zzlesia77@gmail.com>
2023-03-19 21:00:45 +0000

    First commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const Router = require("./routes/api/contacts");
-//mongodb+srv://Olesia:Olesia:zaysella09061982@cluster0.oqvrra4.mongodb.net/?retryWrites=true&w=majority
-
 require("dotenv").config();
+const contactsRouter = require("./routes/api/contacts");
+const authRouter = require("./routes/api/auth");
+
+//mongodb+srv://Olesia:Olesia:zaysella09061982@cluster0.oqvrra4.mongodb.net/?retryWrites=true&w=majority
 
 const app = express();
 
@@ -14,7 +15,9 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", Router);
+app.use("/api/contacts", contactsRouter);
+
+app.use("/api/users", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
